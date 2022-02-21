5eeda01ee2748d73480aa7188da6a570acf1586b
Joswe <josue.espinozagtr@uanl.edu.mx>
2022-02-21 17:56:39 -0600

    Cambiar vista de registro

--- main.js
+++ main.js
@@ -22,3 +22,53 @@ function closeModal(){
     modal.style.display = 'none';
 
 }
+
+// // Validaciones
+const form = document.getElementById('form-registro');
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const nacimiento = document.getElementById('nacimiento');
+const correo = document.getElementById('correo');
+const fotoPerfil = document.getElementById('fotoPerfil');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const btnConfirmarRegs= document.getElementById('btnConfirmarRegs');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+    const nombreValor = username.value.trim();
+    const apellidoValor = username.value.trim();
+    const nacimientoValor = username.value.trim();
+    const correoValor = username.value.trim();
+    const usernameValue = username.value.trim();
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+}
